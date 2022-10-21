4c9acddc75b06e4709644277ac09adbbe37c039c
Alice-anjali <alice.anjali1995@gmail.com>
2022-10-21 16:08:53 +0530

    add code for points

--- PiMonteCarlo.py
+++ PiMonteCarlo.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 #1. Circle at origin with radius 1
 def draw_circle():
@@ -15,5 +16,41 @@ def draw_circle():
     plt.ylim([-1.3, 1.3])
 
 
-draw_circle()
-plt.show()
+#Scaling the random point of 0,1 range to -1,1 range using Simple Linear Conversion
+
+def get_new_value(k):
+    oldMin = 0
+    oldMax = 1
+    newMin = -1
+    newMax = 1
+    oldValue = k #received random value
+
+    oldRange = oldMax - oldMin
+    newRange = newMax - newMin
+    newValue = (((oldValue - oldMin) * newRange) / oldRange) + newMin #calculated new value
+
+    return newValue
+
+
+#2 plot random points
+
+def plot_points(points):
+    
+    #list to store x and y values of the points
+    x_list = []
+    y_list = []
+    for i  in range(points):
+        #generating random x and y values
+        x = np.random.rand()
+        y = np.random.rand()
+        new_x = round(get_new_value(x), 1)
+        new_y = round(get_new_value(y), 1)
+                
+        x_list.append(new_x)
+        y_list.append(new_y)
+        
+    draw_circle() #plotting circle
+    plt.plot(x_list, y_list, 'o', color='r' ) #plotting points
+    plt.show()
+    
+    return x_list, y_list
