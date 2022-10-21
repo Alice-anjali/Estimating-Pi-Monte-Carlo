d0ac1c50c1e443183e6c0abf16d7ae95924ba959
Alice-anjali <alice.anjali1995@gmail.com>
2022-10-21 16:04:40 +0530

    add code for circle

--- PiMonteCarlo.py
+++ PiMonteCarlo.py
@@ -1,1 +1,19 @@
-print("Monte Carlo")
+import matplotlib.pyplot as plt
+
+#1. Circle at origin with radius 1
+def draw_circle():
+    fig, ax = plt.subplots()
+    x = 0
+    y = 0
+    radius = 1 #radius of circle
+    circle = plt.Circle( (x, y ),radius ,fill = False, color='b' )
+
+    ax.set_aspect(1)
+    ax.add_artist(circle)
+    plt.title( 'Circle' )
+    plt.xlim([-1.3, 1.3])
+    plt.ylim([-1.3, 1.3])
+
+
+draw_circle()
+plt.show()
